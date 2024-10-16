a9facfd5d6a501a146ce5f807e08a16c4ff7aaab
Charu <charugupta.217740@gmail.com>
2024-10-16 23:15:16 +0530

    Update prac_ch7.py

--- prac_ch7.py
+++ prac_ch7.py
@@ -1,7 +1,7 @@
-# #ques1 print table of a number
-# num=int(input("Enter the number to print table of: "))
-# for i in range (1,11,1):
-#     print(f"{num}*{i} = {num*i})
+#ques1 print table of a number
+num=int(input("Enter the number to print table of: "))
+for i in range (1,11,1):
+   print(f"{num}*{i} = {num*i})
 
 #ques1 using while loop
 num=int(input("Enter the number to print table of: "))
