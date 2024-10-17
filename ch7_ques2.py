5bf706227f06b990cd063363fa4c7780d8027d01
Charu <charugupta.217740@gmail.com>
2024-10-17 22:21:37 +0530

    Update ch7_ques2.py

---
+++ ch7_ques2.py
@@ -0,0 +1,8 @@
+# ques 2 whether given number is prime or not
+n=int(input("Enter the number: "))
+for i in range (2,n):
+    if(n%i==0):
+        print("Number is not prime")
+        break
+else:
+    print("Number is prime")
