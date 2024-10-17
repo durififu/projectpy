1a23a146bbfabca04c1843f19359d6d23136df43
Charu <charugupta.217740@gmail.com>
2024-10-17 22:23:08 +0530

    Update patternques.py

--- patternques.py
+++ patternques.py
@@ -8,3 +8,14 @@ for i in range (1, n+1):
     print(" "*(n-i), end="") #for printing spaces at the starting
     print("*"*(2*i-1), end="")
     print("")
+ 
+''' printing pattern
+*
+**
+***
+'''
+n=int(input("Enter the number: "))
+for i in range(1,n+1):
+    print("*"*i, end="")
+    print("")
+ 
