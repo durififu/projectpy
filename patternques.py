0f5fde976675719ec52c37a7b6d73c1d5ef5675e
Charu <charugupta.217740@gmail.com>
2024-10-18 06:03:41 +0530

    Update patternques.py

--- patternques.py
+++ patternques.py
@@ -1,4 +1,4 @@
-''' print the pattern without using loops
+''' PATTERN 1 print the pattern without using loops
 for n=3
   *
  ***
@@ -9,7 +9,7 @@ for i in range (1, n+1):
     print("*"*(2*i-1), end="")
     print("")
  
-''' printing pattern
+''' PATTERN 2 printing pattern
 *
 **
 ***
@@ -19,3 +19,17 @@ for i in range(1,n+1):
     print("*"*i, end="")
     print("")
  
+'''  PATTERN 3 printing pattern
+***
+* *
+***
+'''
+n=int(input("Enter the number of rows: "))
+for i in range(1,n+1):
+    if(i==1 or i==n):  # for rows
+        print("*"*n, end='')
+    else:
+        print("*", end='')
+        print(" "*(n-2), end='')
+        print("*", end='')
+    print("") #used to print new line
