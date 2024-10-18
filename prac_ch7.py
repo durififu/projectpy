4957f2d0d45d25981a06a4ab2ace0bcb2db61dd6
Charu <charugupta.217740@gmail.com>
2024-10-18 06:11:41 +0530

    Update prac_ch7.py

--- prac_ch7.py
+++ prac_ch7.py
@@ -9,3 +9,13 @@ i=1
 while(i<11):
     print(f"{num}*{i} = {num*i}")
     i+=1
+
+# Print the multiplication table in reverse order METHOD 1
+num=int(input("Enter the number to print table of: "))
+for i in range (10,0,-1):
+    print(f"{num}*{i} = {num*i}")
+
+# Print the multiplication table in reverse order METHOD 2
+num=int(input("Enter the number to print table of: "))
+for i in range (1, 11):
+    print(f"{num}*{11-i} = {num*(11-i)}")  
