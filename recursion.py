b5c101d2dcfd90673628cee1d54d5410be7028af
Charu <charugupta.217740@gmail.com>
2024-10-20 22:00:39 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -5,3 +5,12 @@ def fact(n):
     return n * fact(n-1)
 n=int(input("Enter the number: "))
 print(f"The factorial is {fact(n)}")
+
+#Write a recursive function to print sum of n natural numbers
+def sum(n):
+    if(n==1):
+        return 1
+    return sum(n-1)+n
+num=int(input("Enter the number: "))
+a=sum(num)
+print(f"The sum of {num} natural numbers is {a}")
